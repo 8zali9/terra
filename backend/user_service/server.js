3804e51bbb8274cb71891fb26315799d3c6e531f
Zulfiqar Ali <8zali9@gmail.com>
2024-05-11 08:01:08 +0500

    server concurrency - clusters

--- backend/user_service/server.js
+++ backend/user_service/server.js
@@ -1,3 +1,5 @@
+const cluster = require('node:cluster')
+const cores = require('node:os').availableParallelism()
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
@@ -6,23 +8,30 @@ const userApi = require('./api/userApi')
 
 const port = process.env.PORT
 const env = process.env.ENV
-const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-}))
+if (cluster.isPrimary) {
+    for (let i = 0; i < cores; i++) {
+        cluster.fork()
+    }
+} else {
+    const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/terra.user-service/', userApi)
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true
+    }))
 
-connect().then(() => {
-    app.listen(port, () => {
-        console.log(`user_service@${env}:${port}`)
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use('/terra.user-service/', userApi)
+
+    connect().then(() => {
+        app.listen(port, () => {
+            console.log(`user_service@${env}:${port}`)
+        })
+    }).catch(() => {
+        console.log(`error running user_service!`)
+        process.exit(1)
     })
-}).catch(() => {
-    console.log(`error running user_service!`)
-    process.exit(1)
-})
+}
